a27df7d8abdc304a835965fd44f4b75373e9baca
msilva <marcos_2802@terra.com.br>
2020-02-14 19:38:39 -0300

    Implementing REST Calculator - SUM Operation

---
+++ RestWithASP.NET-Udemy 01 - Calculator/RestWithASP.NET-Udemy 00 - Scafold/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASP.NET_Udemy_00___Scafold.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(result);
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, /*System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo,*/ out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            decimal result;
+
+            return decimal.TryParse(number, /*System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo,*/ out result);
+        }
+    }
+}
